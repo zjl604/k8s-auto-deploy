7d265da9f959b01a96c2f680b2f910746897a571
zjl604 <morningsir108@163.com>
2018-09-11 16:19:23 +0800

    Update 自签证书.sh

--- 自签证书.sh
+++ 自签证书.sh
@@ -1,35 +1,10 @@
-##### etcd  
-* ca.pem （ca数字证书）
-* server.pem (server数字证书-服务端证书)
-* server-key.pem（带key的就是私钥，用于加减秘）
+#!/bin/bash
+##### 证书生成工具 cfssl 部署：
+wget -P /usr/local/bin/ -O "cfssl" https://pkg.cfssl.org/R1.2/cfssl_linux-amd64  
+wget -P /usr/local/bin/ -O "cfssljson" https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64  
+wget -P /usr/local/bin/ -O "cfssl-certinfo" https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64  
+chmod +x /usr/local/bin/*
 
-##### kube-apiserver
-* ca.pem
-* server.pem
-* server-key.pem
-
-##### kubelet
-* ca.pem
-* ca-key.pem
-
-##### kube-proxy
-* ca.pem
-* kube-proxy.pem
-* kube-proxy-key.pem
-
-##### kubectl（客户端访问集群，作为集群管理员，通过这个证书访问 api）
-* ca.pem
-* admin.pem
-* admin-key.pem 
-
-##### 安装证书生成工具 cfssl：
-wget https://pkg.cfssl.org/R1.2/cfssl_linux-amd64  
-wget https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64  
-wget https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64  
-chmod +x cfssl_linux-amd64 cfssljson_linux-amd64 cfssl-certinfo_linux-amd64  
-mv cfssl_linux-amd64 /usr/local/bin/cfssl  
-mv cfssljson_linux-amd64 /usr/local/bin/cfssljson  
-mv cfssl-certinfo_linux-amd64 /usr/bin/cfssl-certinfo
 
 生成证书的一个模板文件:  
 cfssl print-defaults config > config.json  
