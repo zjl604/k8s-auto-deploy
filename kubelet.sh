469ecbf00e850b85f71a7dc0adfcf3a4f275d4e7
zjl604 <morningsir108@163.com>
2018-09-11 19:19:54 +0800

    Update kubelet.sh

--- kubelet.sh
+++ kubelet.sh
@@ -1,1 +1,21 @@
-cat 
+cat <<"EOF" >/usr/lib/systemed/system/kubelet.service
+[Unit]
+Description=Kubernetes Kubelet
+Documentation=https://github.com/GoogleCloudPlatform/kubernetes
+After=docker.service
+Requires=docker.service
+
+[Service]
+WorkingDirectory=/var/lib/kubelet
+ExecStart=kubelet \
+  --hostname-override=k8s-node01 \
+  --pod-infra-container-image=jicki/pause-amd64:3.1 \
+  --bootstrap-kubeconfig=/opt/kubernetes/cfg/k8s-node01-bootstrap.kubeconfig \
+  --kubeconfig=/etc/kubernetes/kubelet.kubeconfig \
+  --config=/etc/kubernetes/kubelet.config.json \
+  --cert-dir=/etc/kubernetes/ssl \
+  --logtostderr=true \
+  --v=2
+
+[Install]
+WantedBy=multi-user.target
